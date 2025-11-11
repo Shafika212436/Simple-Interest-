af675d0821e7438a1e5136968efa860362dff64b
shafika <Shafikakhanum95@gmail.com>
2025-11-12 00:34:10 +0530

    save

---
+++ simpleintrest.py
@@ -0,0 +1,10 @@
+# simple_intrest.py
+# program to calculate simple intrest
+
+principal = 10000
+rate = 9
+time = 5
+
+simple_intrest = (principal * rate * time)/100
+
+print(f"\n simple intrest = {simple_intrest:.2f}")
